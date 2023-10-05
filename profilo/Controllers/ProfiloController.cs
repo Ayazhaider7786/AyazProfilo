e6a8388eedd595fd278aa9cbec74ee9a74e87dc3
Ayazhaider7786 <ayazhaider7786@gmail.com>
2023-10-05 22:48:05 +0500

    Initial commit

---
+++ profilo/Controllers/ProfiloController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace profilo.Controllers
+{
+    public class ProfiloController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+    }
+}
